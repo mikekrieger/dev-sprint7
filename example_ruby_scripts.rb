d5ab531c56e901f932a2e9705a9dae752bebc5f8
mikekrieger <mlkrgr@gmail.com>
2013-09-23 21:09:30 -0400

    readme

--- example_ruby_scripts.rb
+++ example_ruby_scripts.rb
@@ -1,8 +1,10 @@
 class TriangleError < StandardError
 end
+nil
 
 class PalindromeError < StandardError
 end
+nil
 
 def is_triangle? args
   raise ArgumentError, "There are not 3 arguments" if args.length != 3
@@ -11,6 +13,19 @@ def is_triangle? args
   raise TriangleError, "This is not a triangle" unless max <= array[0] + array[1]
   puts "Congratulations, you have found a triangle!"
 end
+irb(main):077:0> is_triangle? [1,2,3]
+Congratulations, you have found a triangle!
+=> nil
+irb(main):078:0> is_triangle? [1,9,10]
+Congratulations, you have found a triangle!
+=> nil
+irb(main):079:0> is_triangle? [1,2,10]
+TriangleError: This is not a triangle
+  from (irb):74:in `is_triangle?'
+  from (irb):79
+  from /usr/local/bin/irb:12:in `<main>'
+irb(main):080:0> 
+
 
 def another_is_triangle_method args
   if args.length != 3
@@ -24,11 +39,25 @@ def another_is_triangle_method args
     puts "Congratulations, you have found a triangle!"
   end
 end
-
+nil
+irb(main):092:0> another_is_triangle_method [1,9,10]
+Congratulations, you have found a triangle!
+=> nil
+irb(main):093:0> another_is_triangle_method [1,2,10]
+NoMethodError: undefined method `TriangleError' for main:Object
+  from (irb):87:in `another_is_triangle_method'
+  from (irb):93
+  from /usr/local/bin/irb:12:in `<main>'
+irb(main):094:0> another_is_triangle_method [1,1,1]
+Congratulations, you have found a triangle!
+=> nil
 
 def reverse_string string
   string.reverse
 end
+irb(main):100:0> reverse_string [1,2,3,4]
+=> [4, 3, 2, 1]
+
 
 def tedious_reverse_string string
   arr, reverse_arr = [], []
@@ -37,6 +66,7 @@ def tedious_reverse_string string
     reverse_arr << arr.pop
   end
 end
+nil
 
 def is_palindrome blob
   array = blob.to_s.chars.to_a
@@ -51,3 +81,4 @@ def is_palindrome blob
     raise PalindromeError, "Not a Palindrome"
   end
 end
+nil
